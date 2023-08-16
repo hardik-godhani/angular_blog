0e90a2be7813e64f788da498d50dd870b6046663
hardik-godhani <hardikgodhani6@gmail.com>
2023-08-16 10:37:45 +0530

    observables, map and filter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +9,31 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'angular_blog';
   showContent = true;
+  subs = new Subscription();
+
+  observable = new Observable((observer) => {
+    observer.next('Hello');
+    observer.next('Savalan');
+    setTimeout(() => {
+      observer.next('time out of 1.5 sec');
+    }, 1500);
+    observer.complete();
+  });
+
+  constructor() {
+
+    this.subs.add(
+      this.observable.subscribe((value) => {
+        console.log('log from constructor : ', value);
+      })
+    )
+
+    this.observable.subscribe((value) => {
+      console.log('from subs 2 log from constructor : ', value);
+    })
+  }
+
+  ngOnDestroy() {
+    this.subs.unsubscribe();
+  }
 }
--- src/app/blog-list/blog-list.component.ts
+++ src/app/blog-list/blog-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import { BlogComponent } from './blog/blog.component';
+import { Observable, filter, map, of } from 'rxjs';
 
 @Component({
   selector: 'app-blog-list',
@@ -8,6 +9,42 @@ import { BlogComponent } from './blog/blog.component';
 })
 export class BlogListComponent {
   @ViewChild('app_blog') header: BlogComponent = new BlogComponent();
+
+  numberObservable = of(1,2,3,4,5,6,7,8,9);
+  // numberObservable = of(1, 2);
+  // numberObservable = new Observable((observer) => {
+  //   observer.next(1);
+  //   observer.next(2);
+  //   observer.next(3);
+  //   observer.next(4);
+  //   observer.next(5);
+  //   observer.next(6);
+  //   observer.next(7);
+  //   observer.next(8);
+  //   observer.next(9);
+  //   observer.complete();
+  // });
+
+  constructor() {
+    // MAP
+    this.numberObservable.pipe(
+      map((value) => value * value * value)
+    ).subscribe((value) => {
+      console.log('observable values : ', value);
+    })
+
+    // Filter
+    this.numberObservable.pipe(
+      filter((value) => value % 2 === 1)
+    ).subscribe((value) => {
+      console.log('odd values : ', value);
+    })
+  }
+
+  // take
+  // takeuntil
+  // mergeMap
+  // SwitchMap
 
   showBlog = true;
   isLoggedIn = false;
